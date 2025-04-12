08af2297d11749287308d669ea1a70f1d6b58611
Kidus-Sofonias <sofoniaskidus@gmail.com>
2025-04-12 15:13:15 +0300

    nav logo

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./Nav.css"; // Import the CSS module
-import logo from "../../assets/images/logo.png";
+import "./Nav.css";
+import logo from "../../assets/images/nav-logo.png";
 
 function Nav() {
   return (
